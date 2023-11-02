73b985b45fb24b05d9d6ddd6f0c4f2fcf1b175c8
Julian <julianshen2002@yahoo.de>
2023-11-02 09:34:48 +0100

    Rewrite detection method for uniform superposition.

---
+++ utils.py
@@ -0,0 +1,45 @@
+from io import TextIOWrapper
+
+
+class FileReader:
+
+    def __init__(self, file: TextIOWrapper) -> None:
+        self.file: TextIOWrapper = file
+        self.lines: list = self.file.readlines()
+        self.file.seek(0)
+
+    def read_file_until(self, target: int) -> str:
+
+        if target > len(self.lines):
+            return self.file.read()
+
+        output: str = ""
+
+        for line_num in range(0, target + 1):
+            output += self.lines[line_num]
+
+        return output
+
+
+# Calculates all possible combinations of values in the input list.
+def get_combinations(input_list: list):
+    masks = [1 << i for i in range(len(input_list))]
+    for i in range(1 << len(input_list)):
+        yield [ss for mask, ss in zip(masks, input_list) if i & mask]
+
+
+def all_equal(list: list, indices: list):
+    to_compare = list[indices[0]]
+
+    for i in indices:
+        if list[i] != to_compare:
+            return False
+
+    return True
+
+
+def convert_to_int(list):
+    for i in range(0, len(list)):
+        list[i] = int(list[i], 2)
+
+    return list
